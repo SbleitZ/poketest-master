579534633282a3f3910df20b7606cf288d54c469
Sbleit <zptrack24@gmail.com>
2022-12-22 13:24:24 -0300

    fix header

--- src/componentes/SideBar.jsx
+++ src/componentes/SideBar.jsx
@@ -8,6 +8,9 @@ function SideBar(){
       <h2 className='SideBar-title'>Categorias</h2>
       <ul className='SideBar-main-lista'>
         <li><Link to="/categorias/fire">Fire</Link></li>
+        <li><Link to="/categorias/Water">Water</Link></li>
+        <li><Link to="/categorias/grass">Grass</Link></li>
+        <li><Link to="/categorias/psychic">psychic</Link></li>
         <li><Link to="/categorias/Electric">Electric</Link></li>
         <li><Link to="/categorias/Bug">Bug</Link></li>
         <li><Link to="/categorias/Normal">Normal</Link></li>
